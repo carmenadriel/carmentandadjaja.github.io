eabd6762c466a9fd60f8c7160ce576b975bc3e40
Carmen Tandadjaja <carmen.tandadjaja@gmail.com>
2019-08-11 20:38:33 +0700

    Added Pokemon API project
    Updated website to include Pokemon API project

---
+++ my-pokemon/precache-manifest.400690c9d0cceccb6491384bd09b7282.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abedd3b6453390b2e3ffc34249867764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b62ed1755de54d5a886",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "908c850ad92fa7a7355c",
+    "url": "/static/css/main.88ecdea1.chunk.css"
+  },
+  {
+    "revision": "4b62ed1755de54d5a886",
+    "url": "/static/js/2.7988f68e.chunk.js"
+  },
+  {
+    "revision": "908c850ad92fa7a7355c",
+    "url": "/static/js/main.c15885c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dff32475c16f243bc81f50dbce0481dc",
+    "url": "/static/media/Pokeball.dff32475.png"
+  }
+]);
